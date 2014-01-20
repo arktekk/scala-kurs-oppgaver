92cefdea8b35bfba5be639650561a592ec771a37
Jon-Anders Teigen <jteigen@gmail.com>
2014-01-20 11:23:03 +0100

    sql eksempel

--- sql/src/main/scala/sql/Jdbc.scala
+++ sql/src/main/scala/sql/Jdbc.scala
@@ -4,14 +4,37 @@ import org.springframework.jdbc.core.{RowMapper, JdbcTemplate}
 import java.sql.ResultSet
 import collection.JavaConverters._
 
+trait Session{
+  def rollback = ???
+  def commit = ???
+}
+
 class Jdbc(jdbc:JdbcTemplate) {
 
   implicit class SqlContext(sc:StringContext){
-    def sql(params:Any*):Sql = ???
+    def sql(params:Any*):Sql = new Sql(sc.parts.mkString("?"), params.map(_.asInstanceOf[AnyRef]))
   }
 
-  class Sql(ps:String, params:Seq[Any]){
-    def update:Unit = ???
-    def mapRow[A](f:(ResultSet, Int) => A):List[A] = ???
+
+
+
+  def tx[A](f:Session => A):A = {
+    // tx start
+    try {
+      f(new Session{})
+      // tx commit
+    } catch {
+      case ex =>
+        //rollback
+      throw ex
+    }
+  }
+
+  class Sql(ps:String, params:Seq[AnyRef]){
+    def update(implicit tx:Session):Unit = jdbc.update(ps, params :_*)
+    def mapRow[A](f:ResultSet => A)(implicit tx:Session):List[A] =
+      jdbc.query(ps, params.toArray, new RowMapper[A] {
+        def mapRow(rs: ResultSet, rowNum: Int): A = f(rs)
+      }).asScala.toList
   }
 }
--- sql/src/test/scala/sql/Demo.scala
+++ sql/src/test/scala/sql/Demo.scala
@@ -8,14 +8,23 @@ object Demo {
     import org.springframework.jdbc.datasource.DriverManagerDataSource
 
     Class.forName("org.h2.Driver")
-    val app = new DemoApp(new Jdbc(new JdbcTemplate(new DriverManagerDataSource("jdbc:h2:mem:demo;DB_CLOSE_DELAY=-1"))))
+    val jdbc = new Jdbc(new JdbcTemplate(new DriverManagerDataSource("jdbc:h2:mem:demo;DB_CLOSE_DELAY=-1")))
+    val app = new DemoApp(jdbc)
+
+    jdbc.tx{ implicit tx =>
 
     app.create
     app.insert("Mr A")
     app.insert("Mr B")
     app.insert("Mr C")
 
-    app.list.foreach(println)
+    }
+
+    jdbc.tx{ implicit tx =>
+      app.list.foreach(println)
+    }
+
+
   }
 
 }
@@ -25,9 +34,9 @@ case class User(id:Int, name:String)
 class DemoApp(jdbc:Jdbc){
   import jdbc._
 
-  def create = sql"create table user (id int primary key auto_increment, name varchar(50));".update
+  def create(implicit s:Session) = sql"create table user (id int primary key auto_increment, name varchar(50));".update
 
-  def insert(name:String) = sql"insert into user (name) values($name);".update
+  def insert(name:String)(implicit s:Session) = sql"insert into user (name) values($name);".update
 
-  def list = sql"select id, name from user".mapRow((rs, i) => User(rs.getInt("id"), rs.getString("name")))
+  def list(implicit s:Session) = sql"select id, name from user".mapRow(rs => User(rs.getInt("id"), rs.getString("name")))
 }
